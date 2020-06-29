6082433b8fafc2bd817b457b767b82374fd12ac9
noleafnolife <770829892@qq.com>
2020-06-29 09:16:14 +0800

    update fg

---
+++ DoomSRP2019/Assets/DoomSRP/fg/RenderTaskBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoomSRP.FG
+{
+    public class RenderTaskBase
+    {
+        protected string name = "";
+        protected bool cullImmune = false;
+        protected List<ResourceBase> creates = new List<ResourceBase>();
+        protected List<ResourceBase> reads = new List<ResourceBase>();
+        protected List<ResourceBase> writes = new List<ResourceBase>();
+        protected UInt32 refCount = 0;
+
+        public string Name { get { return this.name; } set { this.name = value; } }
+        public bool CullImmune { get { return this.cullImmune; } set { this.cullImmune = value; } }
+
+
+        public RenderTaskBase(string name)
+        {
+            this.name = name;
+        }
+
+        
+    }
+}
---
+++ DoomSRP2019/Assets/DoomSRP/fg/ResourceBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoomSRP.FG
+{
+    public class ResourceBase
+    {
+        protected UInt32 id;
+        protected string name;
+        protected RenderTaskBase creator;
+        protected List<RenderTaskBase> readers;
+        protected List<RenderTaskBase> writers;
+        protected UInt32 refCount;
+
+
+
+    }
+}
